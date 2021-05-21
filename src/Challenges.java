851ac8ab2ce75e81236336d5b44f98b87e35cc5d
JosephMugo <joemu18@gmail.com>
2021-05-21 14:27:58 -0500

    Implemented All Algorithms

--- src/Challenges.java
+++ src/Challenges.java
@@ -1,16 +1,38 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+
 public class Challenges {
-    /** Create a function which returns the number of true values there are in an array.*/
+    /**
+     * Create a function which returns the number of true values there are in an array.
+     */
     public static int countTrueFalse(boolean[] arr) {
-        // write your code here
-        // you will need to change the return statement
-        return Integer.parseInt(null);
+        int amountOfTrue = 0;
+        for (int x = 0; x < arr.length; x++) {
+            if (arr[x] == true) {
+                amountOfTrue++;
+            }
+        }
+        return amountOfTrue;
     }
 
-    /** Create a function that takes an array and returns the absolute difference between the biggest and smallest numbers.*/
-    public static int differenceMinMax(int[] nums){
-        // write your code here
-        // you will need to change the return statement
-        return Integer.parseInt(null);
+    /**
+     * Create a function that takes an array and returns the absolute difference between the biggest and smallest numbers.
+     */
+    public static int differenceMinMax(int[] nums) {
+        int largest = nums[0];
+        int smallest = nums[0];
+
+        for (int x = 0; x < nums.length; x++) {
+            if (nums[x] > largest) {
+                largest = nums[x];
+            }
+            if (nums[x] < smallest) {
+                smallest = nums[x];
+            }
+        }
+        return largest - smallest;
     }
 
     /**
@@ -19,32 +41,58 @@ public class Challenges {
      * The first two letters are repeated twice with an ellipsis ... and space after each,
      * and then the word is pronounced with a question mark ?
      */
-    public static String stutter(String str){
-        // write your code here
-        // you will need to change the return statement
-        return "";
-    }
-
-    /** Write a function that takes an array of numbers and returns the second largest number.*/
-    public static int secondLargestNumber(int[] nums){
-        // write your code here
-        // you will need to change the return statement
-        return Integer.parseInt(null);
+    public static String stutter(String str) {
+        return str.substring(0, 2) + "... " + str.substring(0, 2) + "... " + str + "?";
     }
 
     /**
-     FizzBuzz
-     Create a function that takes a number as an argument and returns "Fizz", "Buzz" or "FizzBuzz".
-     If the number is a multiple of 3 the output should be "Fizz".
-     If the number given is a multiple of 5, the output should be "Buzz".
-     If the number given is a multiple of both 3 and 5, the output should be "FizzBuzz".
-     If the number is not a multiple of either 3 or 5, the number should be output on its own as shown in the examples below.
-     The output should always be a string even if it is not a multiple of 3 or 5.
+     * Write a function that takes an array of numbers and returns the second largest number.
+     */
+    public static int secondLargestNumber(int[] nums) {
+        int largest = nums[0];
+        int smallest = nums[0];
+        // get the largest number
+        for (int x = 0; x < nums.length; x++) {
+            if (nums[x] > largest) {
+                largest = nums[x];
+            }
+            if (nums[x] < smallest) {
+                smallest = nums[x];
+            }
+        }
+        // check for largest number again that is not equal to first largest
+        int secondLargest = smallest;
+        for (int x = 0; x < nums.length; x++) {
+            if (nums[x] > secondLargest && nums[x] != largest) {
+                secondLargest = nums[x];
+            }
+        }
+        return secondLargest;
+    }
+
+    /**
+     * FizzBuzz
+     * Create a function that takes a number as an argument and returns "Fizz", "Buzz" or "FizzBuzz".
+     * If the number is a multiple of 3 the output should be "Fizz".
+     * If the number given is a multiple of 5, the output should be "Buzz".
+     * If the number given is a multiple of both 3 and 5, the output should be "FizzBuzz".
+     * If the number is not a multiple of either 3 or 5, the number should be output on its own as shown in the examples below.
+     * The output should always be a string even if it is not a multiple of 3 or 5.
      **/
-    public static String fizzBuzz(int num){
-        // write your code here
-        // you will need to change the return statement
-        return "";
+    public static String fizzBuzz(int num) {
+        String returnString = "";
+        if (num % 3 == 0) {
+            returnString += "Fizz";
+        }
+        if (num % 5 == 0) {
+            returnString += "Buzz";
+        }
+
+        if ((num % 3 != 0) && (num % 5 != 0)) {
+            return Integer.toString(num);
+        }
+
+        return returnString;
     }
 
     /**
@@ -52,48 +100,80 @@ public class Challenges {
      * Write a program that gets three integers from the user.
      * Count from the first number to the second number in increments of the third number.
      */
-    public static int[] skipCount(int countFrom, int countTo, int countBy){
-        // write your code here
-        // you will need to change the return statement
-        return new int[0];
+    public static String skipCount(int countFrom, int countTo, int countBy) {
+        if (countFrom > countTo) {
+            return "Try again with better numbers!";
+        }
+        int pos = countFrom;
+        String returnString = "";
+        while (pos <= countTo) {
+            returnString += pos;
+            pos += countBy;
+            if (pos <= countTo) {
+                returnString += ".. ";
+            }
+        }
+        return returnString;
     }
 
-    /** The "Reverser"
-     * takes a string as input and returns that string in reverse order, with the opposite case.*/
-    public static String reverser(String reverseMe){
-        // write your code here
-        // you will need to change the return statement
-        return "";
+    /**
+     * The "Reverser"
+     * takes a string as input and returns that string in reverse order, with the opposite case.
+     */
+    public static String reverser(String reverseMe) {
+        String returnString = "";
+        for (int x = reverseMe.length() - 1; x >= 0; x--) {
+            if (Character.isUpperCase(reverseMe.charAt(x))) {
+                returnString += Character.toLowerCase(reverseMe.charAt(x));
+            } else {
+                returnString += Character.toUpperCase(reverseMe.charAt(x));
+            }
+        }
+        return returnString;
     }
 
     /**
      * Reverse An Array
      * Write a function that reverses the order of an array
      */
-    public static int[] reverseArray(int[] arr){
-        // write your code here
-        // you will need to change the return statement
-        return new int[0];
+    public static int[] reverseArray(int[] arr) {
+        int[] reversedArray = new int[arr.length];
+        int y = 0;
+        for (int x = arr.length - 1; x >= 0; x--) {
+            reversedArray[y] = arr[x];
+            y++;
+        }
+        return reversedArray;
     }
 
     /**
      * Write a function that finds the word "bomb" in the given string (not case sensitive).
      * Return "DUCK!" if found, otherwise,"Relax, there's no bomb.".
      */
-    public static String duckTheBomb(String checkMe){
-        // write your code here
-        // you will need to change the return statement
-        return "";
+    public static String duckTheBomb(String checkMe) {
+
+        if (checkMe.toLowerCase().contains("bomb")) {
+            return "DUCK!";
+        }
+        return "Relax, there's no bomb.";
     }
 
-    /** Sort in Ascending Order
+    /**
+     * Sort in Ascending Order
      * Create a method that takes an array of integers and returns a new array, sorted in ascending order (smallest to biggest).
      * If the function's argument is null, an empty array or undefined, return an empty array
      */
-    public static int[] sortNumsAscending(int[] nums){
-        // write your code here
-        // you will need to change the return statement
-        return new int[0];
+    public static int[] sortNumsAscending(int[] nums) {
+        List<Integer> newArray = new ArrayList<>();
+        for (int num : nums) {
+            newArray.add(num);
+        }
+        Collections.sort(newArray);
+        int[] returnArray = new int[newArray.size()];
+        for (int x = 0; x < newArray.size(); x++) {
+            returnArray[x] = newArray.get(x);
+        }
+        return returnArray;
     }
 
     /**
@@ -101,9 +181,18 @@ public class Challenges {
      * Create a method that takes an array of strings,removes all duplicate items
      * and returns a new array in the same sequential order as the old array (minus duplicates).
      */
-    public static String[] removeDups(String[] str){
-        // write your code here
-        // you will need to change the return statement
-        return new String[0];
+    public static String[] removeDups(String[] str) {
+        LinkedHashSet<String> noDups = new LinkedHashSet<>();
+        for (String s : str) {
+            noDups.add(s);
+        }
+        String[] newArray = new String[noDups.size()];
+        int x = 0;
+        for (String item : noDups) {
+            newArray[x] = item;
+            x++;
+        }
+
+        return newArray;
     }
 }
